0e6d7249830b3d87f83cc220ef1e038c816443ed
joshuahoffmann04 <joshuat.hoffmann@t-online.de>
2026-02-03 10:33:35 +0100

    Add chunks

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from pdf_extractor import PDFExtractor
-from dotenv import load_dotenv
+from pdf_extractor import ExtractionResult
+from chunking import DocumentChunker, ChunkingConfig
 
-#.env laden
-load_dotenv()
-
-# Extractor initialisieren
-extractor = PDFExtractor()
-
-# PDF verarbeiten
-def progress(current, total, status):
-    print(f"Seite {current}/{total}: {status}")
-
-result = extractor.extract("pdfs/stpo_bsc-informatik_25-01-23_lese.pdf", progress_callback=progress)
-
-# Ergebnisse nutzen
-print(f"Titel: {result.context.title}")
-print(f"Seiten: {len(result.pages)}")
-
-# In Datei speichern
-result.save("output.json")
+result = ExtractionResult.load("stpo_bsc-informatik_25-01-23_lese.json")
+chunker = DocumentChunker(ChunkingConfig(max_chunk_tokens=512))
+chunks = chunker.chunk(result)
+chunks.save("stpo_bsc-informatik_25-01-23_lese-chunks.json")
